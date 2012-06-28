0987645595ee4c0c35fdedb4252ab315ef736330
Jeff Armstrong <armstrj@leaf>
2012-06-28 22:01:39 +0100

    tweak bootstrap

--- app.php
+++ app.php
@@ -1,14 +1,10 @@
 <?php
+
+  # front-controller...
+
   ini_set("display_errors", 1);
   include_once("./app/bootstrap.php");
-  
-  getInstance('kernel')->run();
-  
-  #getInstance('log')->say(
-  #    $_SERVER['PHP_SELF'],
-  #    " ", $_REQUEST
-  #  );
-  #
-  #phpinfo();
+  include_once("./app/secrets.php");
 
+  getInstance('kernel')->run();
 
